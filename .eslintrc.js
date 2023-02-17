65cce0eeac764aeb2b30909d7642ff97f45da781
Zach Sents <zachsents@gmail.com>
2023-02-17 13:35:22 -0700

    Fixed configs to map to source files properly

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,18 @@
 module.exports = {
     parser: "@babel/eslint-parser",
+    parserOptions: {
+        sourceType: "module",
+    },
     extends: [
         "eslint:recommended",
     ],
-    rules: {
-        "no-extra-boolean-cast": 0,
-    },
     env: {
         browser: true,
         amd: true,
         node: true,
         es6: true,
+    },
+    rules: {
+        "no-extra-boolean-cast": 0,
     },
 }
