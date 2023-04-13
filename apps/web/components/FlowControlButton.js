00b597f502d363b3b6d66666e837e08d11134737
Zach Sents <zachsents@gmail.com>
2023-04-13 13:29:50 -0600

    Made flow control button status showing work better

--- apps/web/components/FlowControlButton.js
+++ apps/web/components/FlowControlButton.js
@@ -1,29 +1,28 @@
 import { ActionIcon, Button, Tooltip } from "@mantine/core"
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import { TbCheck, TbMoodSad } from "react-icons/tb"
 import { useQuery } from "react-query"
 
 
 export default function FlowControlButton({ flow, appId, id, label, icon, small, showStatus, onActivate, bigProps = {}, smallProps = {}, iconSize = "1.2em" }) {
 
-    const { isLoading, isError, isSuccess, refetch } = useQuery({
-        queryKey: ["flowControl", id, flow.id],
-        queryFn: () => onActivate({ flow, appId }),
-        enabled: false,
-        retry: false,
-    })
-
-    // timeout to show status -- starts out as undefined so that it doesn't show on first render
-    const [currentlyShowingStatus, setCurrentlyShowingStatus] = useState()
-    useEffect(() => {
-        if ((isSuccess || isError) && currentlyShowingStatus === false) {
+    // timeout for showing status
+    const [currentlyShowingStatus, setCurrentlyShowingStatus] = useState(false)
+    const setShowStatus = () => {
+        if (showStatus) {
             setCurrentlyShowingStatus(true)
             setTimeout(() => setCurrentlyShowingStatus(false), 2000)
         }
-        else
-            setCurrentlyShowingStatus(false)
-    }, [isSuccess, isError])
+    }
 
+    const { isLoading, isError, isSuccess, refetch } = useQuery({
+        queryKey: ["flowControl", id, flow.id],
+        queryFn: () => onActivate?.({ flow, appId }),
+        enabled: false,
+        retry: false,
+        onSuccess: setShowStatus,
+        onError: setShowStatus,
+    })
 
     const Icon = showStatus && currentlyShowingStatus ? (isSuccess ? TbCheck : isError ? TbMoodSad : icon) : icon
     const color = showStatus && currentlyShowingStatus ? (isSuccess ? "green" : isError ? "red" : "gray") : "gray"
