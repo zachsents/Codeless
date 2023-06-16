1aa310b23a5fb209d6a5f5071f4b655865690a0b
Zach Sents <zachsents@gmail.com>
2023-06-15 19:28:33 -0600

    Made the google-spreadsheet package return typed values in rows

--- server-nodes/google/sheets/shared.js
+++ server-nodes/google/sheets/shared.js
@@ -9,5 +9,23 @@ GoogleSpreadsheetRow.prototype.toString = function () {
     return `Row ${this.rowIndex} in ${this._sheet.title}`
 }
 
+/**
+ * This method is used in GoogleSpreadsheetWorksheet.getRows. This way, it'll default
+ * to using the UNFORMATTED_VALUE valueRenderOption.
+ * 
+ * Note: this may cause issues if a header is a number or boolean and another method
+ * relies on it being a string. This is an unlikely scenario, though. If it happens,
+ * fuck it.
+ */
+GoogleSpreadsheetWorksheet.prototype.getCellsInRange = async function (a1Range, options) {
+    const response = await this._spreadsheet.axios.get(`/values/${this.encodedA1SheetName}!${a1Range}`, {
+        params: {
+            valueRenderOption: "UNFORMATTED_VALUE",
+            majorDimension: "ROWS",
+            ...options,
+        },
+    })
+    return response.data.values
+}
 
 export const MAX_ROWS = 3000
