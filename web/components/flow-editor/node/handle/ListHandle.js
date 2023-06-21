fc03631016b97a982bf769f8b3187a239be3ddd2
Zach Sents <zachsents@gmail.com>
2023-06-21 14:06:50 -0600

    Made unnamed list handles show list label and index on as handle label

--- web/components/flow-editor/node/handle/ListHandle.js
+++ web/components/flow-editor/node/handle/ListHandle.js
@@ -1,13 +1,17 @@
-import { useListHandle } from "@minus/client-nodes/hooks/nodes"
+import { useHandleDefinition, useListHandle } from "@minus/client-nodes/hooks/nodes"
 import Handle from "./Handle"
 
 
 export default function ListHandle({ id, component: Component = Handle, ...props }) {
 
     const [list] = useListHandle(null, id)
+    const { definition } = useHandleDefinition(null, id)
 
-    return list?.map(item => {
+
+    return list?.map((item, i) => {
         const uniqueId = `${id}.${item.id}`
-        return <Component id={uniqueId} label={item.name} {...props} key={uniqueId} />
+        const label = item.name ?? (definition.listNameLabel ? `${definition.listNameLabel} ${i + 1}` : undefined)
+
+        return <Component id={uniqueId} label={label} {...props} key={uniqueId} />
     })
 }
