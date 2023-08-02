6d87ca2adfb6f84f30ff30c877d962be15802775
Zach Sents <zachsents@gmail.com>
2023-08-02 12:00:32 -0600

    Updated ChatGPT node to use GPT-4

--- server-lib/openai/index.js
+++ server-lib/openai/index.js
@@ -26,7 +26,7 @@ export async function createChatCompletion(prompt, {
     checkCharacterLimit(prompt)
 
     const resp = await api().createChatCompletion({
-        model: "gpt-3.5-turbo",
+        model: "gpt-4",
         messages: [{
             role: "user",
             content: prompt,
