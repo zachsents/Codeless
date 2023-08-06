593dd7906f9e06410671ca31b50ad8c6e09d8b60
Zach Sents <zachsents@gmail.com>
2023-08-05 18:11:15 -0600

    Update

--- server-nodes/google/gmail/AddLabel.js
+++ server-nodes/google/gmail/AddLabel.js
@@ -9,6 +9,6 @@ export default {
 
     async onInputsReady({ messageId, label }) {
         const gmailApi = await google.getGoogleAPIFromNode(this, "gmail", "v1")
-        await safeMap((messageId, label) => gmail.modifyLabels(gmailApi, messageId, [label]), messageId, label)
+        await safeMap((messageId, label) => gmail.modifyLabels(gmailApi, messageId, [label], [], false), messageId, label)
     },
 }
