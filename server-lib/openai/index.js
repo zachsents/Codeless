381a59ea04ce605970e9046fe4dd3fb80f07dba1
Zach Sents <zachsents@gmail.com>
2023-08-09 15:46:12 -0600

    Back to gpt-4

--- server-lib/openai/index.js
+++ server-lib/openai/index.js
@@ -26,7 +26,7 @@ export async function createChatCompletion(prompt, {
     checkCharacterLimit(prompt, 8192)
 
     const resp = await api().createChatCompletion({
-        model: "gpt-4-32k",
+        model: "gpt-4",
         messages: [{
             role: "user",
             content: prompt,
